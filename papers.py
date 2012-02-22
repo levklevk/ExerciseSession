2bad3ada0cd9c09d18748956f75766758dfbd47a
Kazakovtsev Lev <levk@bk.ru>
2012-02-22 17:20:00 +0100

    Paper recommendation project started

---
+++ papers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import numpy
+
+class rating(object):
+    def __init__(self):
+        self.ratings=nympy.array([],dtype=int)
+	self.ratings.reshape(0,2)
+	self.people=[]
+	self.papers=[]
+	
+    
+
